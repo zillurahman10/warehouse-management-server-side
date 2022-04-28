f8f62b0488475f2bb6f8b13661838dd345a1d611
zilurahman10 <raihan.brac2019@gmail.com>
2022-04-29 03:06:16 +0600

    mongo db connected

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000
 const app = express()
@@ -7,9 +8,32 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+// products
+// hJ3gtXuVbcuA0Ulr
+
 app.get('/', (req, res) => {
     res.send('All cars are sold! :(')
 })
+
+
+
+const uri = `mongodb+srv://products:hJ3gtXuVbcuA0Ulr@cluster0.gaai3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect()
+
+        const productCollection = client.db("cardotcom").collection("products")
+
+
+
+    }
+    finally {
+
+    }
+}
+
 
 app.listen(port, (req, res) => {
     console.log('Port is running', port);
