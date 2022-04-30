685c8681dc0d135a6416d5facb934e7bd7c4b235
zillurahman10 <raihan.brac2019@gmail.com>
2022-05-01 00:47:01 +0600

    an api created to see  a single product detail

--- index.js
+++ index.js
@@ -33,6 +33,13 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const product = await productCollection.findOne(query)
+            res.send(product)
+        })
+
     }
     finally {
 
